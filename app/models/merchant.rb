c9f4aa0e3c3bdae5e3e8c7680360342007715b02
Patrick Duvall <duvallp208@gmail.com>
2019-06-26 08:48:45 -0600

    Add merchant favorite customer spec

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,4 +2,8 @@ class Merchant < ApplicationRecord
   has_many :items
   has_many :invoices
   has_many :customers, through: :invoices
+
+  def favorite_customer
+    require "pry"; binding.pry
+  end
 end
