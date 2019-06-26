8b6b43f3fcd0cf1f72026847bc84c54b7a814544
Patrick-Duvall <35322570+Patrick-Duvall@users.noreply.github.com>
2019-06-26 17:28:56 -0600

    Update most_revenue_controller.rb

--- app/controllers/api/v1/merchants/most_revenue_controller.rb
+++ app/controllers/api/v1/merchants/most_revenue_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::Merchants::MostRevenueController < ApplicationController
   def index
-    require "pry"; binding.pry
     render json: MerchantSerializer.new(Merchant.most_revenue(params["quantity"]))
   end
 end
