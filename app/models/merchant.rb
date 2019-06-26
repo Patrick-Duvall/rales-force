443ccabfdddb3018c646c8911d9281531eb6ec39
Patrick Duvall <duvallp208@gmail.com>
2019-06-26 15:53:05 -0600

    Remove old method, add comment

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -5,11 +5,7 @@ class Merchant < ApplicationRecord
   has_many :transactions, through: :invoices
 
   def favorite_customer
-    # customers.distinct
-    # .select("COUNT(invoices.id) as invoice_count, customers.*")
-    # .joins(invoices: [:merchant, :transactions])
-    # .where(transactions: {result: "success"}, merchants: {id: self.id})
-    # .group(:id).first
+    #customers does not return distinct
     customers.select("COUNT(invoices.id) as invoice_count, customers.*")
     .joins(:transactions)
     .where(transactions: {result: "success"})
