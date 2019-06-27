6facbfd593ff903c4027452610ebdc2e68df1a79
Patrick Duvall <duvallp208@gmail.com>
2019-06-27 11:24:12 -0600

    Add Customer#favorite_merchant, stand in test and route

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,7 +2,13 @@ class Customer < ApplicationRecord
   has_many :invoices
   has_many :merchants, through: :invoices
   has_many :transactions, through: :invoices
+
+  def favorite_merchant
+    merchants.select("COUNT(invoices.id) as invoice_count, merchants.*")
+    .joins(:transactions)
+    .merge(Transaction.successful)
+    .group('merchants.id')
+    .order(:invoice_count)
+    .last
+  end
 end
-# 
-# Customer.select("COUNT(invoices.id) as invoice_count, customers.*").joins(invoices: [:merchants, :transactions]).where(transa
-# ctions: {result: "success"}, merchants: {id: 4}).group(:id).limit(5)
---
+++ spec/requests/api/v1/customers/favorite_merchant_spec.rb
@@ -0,0 +1,5 @@
+describe "Merchants favorite customer" do
+  xit "works" do
+
+  end
+end
