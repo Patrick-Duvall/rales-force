55be81d0c858038a130aad1b5dda753f9fbdfcff
Patrick Duvall <duvallp208@gmail.com>
2019-06-29 11:34:35 -0600

    Add logic to show date if procided on merchant revenue controller

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::Merchants::RevenueController < ApplicationController
   def show
     merchant = Merchant.find(params["id"])
+    if params['date']
+    render json: RevenueSerializer.new(merchant.total_revenue_on(params['date']))
+    else
     render json: RevenueSerializer.new(merchant.total_revenue)
+  end
   end
 end
