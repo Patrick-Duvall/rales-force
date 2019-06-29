bb8c1f1956719039d2b92245e7cddbe9c8838a65
Patrick Duvall <duvallp208@gmail.com>
2019-06-29 09:23:04 -0600

    Add invoice merchant endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -30,6 +30,7 @@ Rails.application.routes.draw do
        get '/:id/transactions', to: 'transactions#index'
        get '/:id/invoice_items', to: 'invoice_items#index'
        get'/:id/customer', to: 'customer#show'
+       get'/:id/merchant', to: 'merchant#show'
      end
      resources :invoices, only: [:index, :show]
 
